67d2484b69fe967d436f411f69d7b14f7737cd75
ryugeumjung <fbgjung@gmail.com>
2023-08-23 16:37:58 +0900

    쿼리 일부 수정

--- queries/lecture_time.sql
+++ queries/lecture_time.sql
@@ -6,4 +6,4 @@ CREATE TABLE `lecture_time` (
     `lecture_day`
     set('1', '2', '3', '4', '5', '6', '7') NOT NULL,
         `lecture_time` varchar(10) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
