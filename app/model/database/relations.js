4ca45755bebb0e6d3feb7ba06e07f58201e4fea4
jundoopop <jundolpop@gmail.com>
2023-11-05 21:44:57 +0900

    Database - - 약 21~22자리의 String 형태의 PK를 가지는 테이블     - '연도-학기-과목코드-강좌번호' 형태를 가짐     - 예) 2023-01-113031-31001     - newSubjectInformation.js 에 시퀄라이즈 모델 추가     - new_subject_information.sql 파일 추가         - 워크벤치에서 sql 쿼리 실행하고 싶을 때 사용 가능     - iise_subjects_2023_1_REAL.csv 가 새로운 형태에 맞춘 파일로,  파일을 Import 할 수 있음

--- app/model/database/relations.js
+++ app/model/database/relations.js
@@ -1,68 +1,81 @@
 // // relations.js
 
-const ProfessorInformation = require('./professorInformation');
-const SubjectInformation = require('./subjectInformation');
-const LecturePlan = require('./lecturePlan');
-const Basket = require('./basket');
-const IndividualLectures = require('./individualLecture');
-const LectureTime = require('./lectureTime');
-const MajorInformation = require('./majorInformation');
-const StudentIdentities = require('./studentIdentities');
+const ProfessorInformation = require("./professorInformation");
+const SubjectInformation = require("./subjectInformation");
+const LecturePlan = require("./lecturePlan");
+const Basket = require("./basket");
+const IndividualLectures = require("./individualLecture");
+const LectureTime = require("./lectureTime");
+const MajorInformation = require("./majorInformation");
+const StudentIdentities = require("./studentIdentities");
 
 // ProfessorInformation 모델과 SubjectInformation 모델 간의 1:N 관계 설정
 ProfessorInformation.hasMany(SubjectInformation, {
-  foreignKey: 'professor_id',
-  sourceKey: 'professor_id',
+  foreignKey: "professor_id",
+  sourceKey: "professor_id",
 });
+
+// SubjectInformation 모델과
+// MajorInformation 모델 간의 1:N 관계 설정 
+
+// MajorInformation.hasMany(SubjectInformation, {
+//   foreignKey: "created_college",
+//   sourceKey: "major_code",
+// });
+
+// MajorInformation.hasMany(SubjectInformation, {
+//   foreignKey: "governed_college",
+//   sourceKey: "major_code",
+// });
 
 // ProfessorInformation 모델과 LecturePlan 모델 간의 1:N 관계 설정
 ProfessorInformation.hasMany(LecturePlan, {
-  foreignKey: 'professor_id',
-  sourceKey: 'professor_id',
+  foreignKey: "professor_id",
+  sourceKey: "professor_id",
 });
 
 // 강의계획서, 교과목 정보 1:1 관계 설정
 LecturePlan.belongsTo(SubjectInformation, {
-  foreignKey: 'subject_code',
-  sourceKey: 'subject_code'
-})
+  foreignKey: "subject_code",
+  sourceKey: "subject_code",
+});
 
 LecturePlan.belongsTo(SubjectInformation, {
-  foreignKey: 'subject_number',
-  sourceKey: 'subject_number'
-})
+  foreignKey: "subject_number",
+  sourceKey: "subject_number",
+});
 LecturePlan.belongsTo(SubjectInformation, {
-  foreignKey: 'subject_year',
-  sourceKey: 'subject_year'
-})
+  foreignKey: "subject_year",
+  sourceKey: "subject_year",
+});
 LecturePlan.belongsTo(SubjectInformation, {
-  foreignKey: 'subject_semester',
-  sourceKey: 'subject_semester'
-})
+  foreignKey: "subject_semester",
+  sourceKey: "subject_semester",
+});
 
 // 학생, 전공 정보 (주전공) 관계 설정
 StudentIdentities.belongsTo(MajorInformation, {
-  foreignKey : 'student_main_major',
-  sourceKey : 'major_code',
-})
+  foreignKey: "student_main_major",
+  sourceKey: "major_code",
+});
 
 // 학생, 전공 정보 (복수전공) 관계 설정
 StudentIdentities.belongsTo(MajorInformation, {
-  foreignKey : 'student_dual_major',
-  sourceKey : 'major_code',
-})
+  foreignKey: "student_dual_major",
+  sourceKey: "major_code",
+});
 
 // 학생, 전공 정보 (부전공) 관계 설정
 StudentIdentities.belongsTo(MajorInformation, {
-  foreignKey : 'student_sub_major',
-  sourceKey : 'major_code',
-})
+  foreignKey: "student_sub_major",
+  sourceKey: "major_code",
+});
 
 // 학생 1명당 장바구니 1개
 StudentIdentities.hasOne(Basket, {
-  foreignKey : 'student_number',
-  sourceKey : 'student_number',
-})
+  foreignKey: "student_number",
+  sourceKey: "student_number",
+});
 
 // 장바구니,교과목 N:M
 // Basket.hasMany(SubjectInformation, {
@@ -86,12 +99,16 @@ StudentIdentities.hasOne(Basket, {
 //   sourceKey: 'subject_semester',
 // });
 
-Basket.belongsToMany(SubjectInformation, { through: 'BasketSubject', as: 'BasketSubjects' });
-SubjectInformation.belongsToMany(Basket, { through: 'BasketSubject', as: 'BasketSubjects' });
-
+Basket.belongsToMany(SubjectInformation, {
+  through: "BasketSubject",
+  as: "BasketSubjects",
+});
+SubjectInformation.belongsToMany(Basket, {
+  through: "BasketSubject",
+  as: "BasketSubjects",
+});
 
 // 강의정보, 강의시간 1:1
-
 
 module.exports = {
   ProfessorInformation,
@@ -103,4 +120,3 @@ module.exports = {
   MajorInformation,
   StudentIdentities,
 };
-
